cce96eec0f536abef6a5b989cc51e6f4adb3a176
yagyanssh <yagyansh9999@gmail.com>
2024-11-12 14:13:34 +0530

    fixed bugs

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -6,7 +6,7 @@ function Header() {
     <header className="w-full md:px-8 shadow-[0px_0px_10px_0px_rgba(0,_0,_0,_0.11)]">
       
       <nav className="flex justify-between py-4 items-center">
-        <div className="logo font-mono text-black text-2xl font-black tracking-tighter text-transparent min-[375px]:block">
+        <div className="logo font-mono text-black text-2xl font-black tracking-tighter min-[375px]:block">
           Snake_AO
         </div>
         <div>
